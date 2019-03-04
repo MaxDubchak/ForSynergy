1e9e97391810815fab77abc9cf269e9145ec29d5
Max <mrbrainnygg@gmail.com>
2019-03-04 19:10:54 +0200

    Add custom authentication middleware
    add middleware to check wheter user has right to access url
    add custom middleware to django settigns

--- ForSynergy/ForSynergy/settings.py
+++ ForSynergy/ForSynergy/settings.py
@@ -50,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'custom_middleware.custom_auth_middleware.CustomAuthMiddleware',
 ]
 
 ROOT_URLCONF = 'ForSynergy.urls'
---
+++ ForSynergy/custom_middleware/custom_auth_middleware.py
@@ -0,0 +1,35 @@
+"""This module implements custom authentication middleware."""
+
+from django.http import HttpResponse
+
+GUEST_PATHS = [
+    '/user/login',
+    '/user/register',
+]
+
+
+class CustomAuthMiddleware:
+    """
+    Custom middleware that performs authentication validations for cases when
+    the path is not available for anonymous users and logged in user.
+    """
+
+    def __init__(self, get_response):
+        """Initialize middleware instance."""
+        self.get_response = get_response
+
+    def __call__(self, request):
+        """Provide authentication validations on middleware call."""
+
+        for path in GUEST_PATHS:
+            if request.path_info.startswith(path):
+                if request.user.is_authenticated:
+                    return HttpResponse('Unavailable for authenticated users', status=403)
+                response = self.get_response(request)
+                return response
+
+        if not request.user.is_authenticated:
+            return HttpResponse('Unavailable for unavailable for unauthenticated users', status=403)
+
+        response = self.get_response(request)
+        return response
